4d3e4beb1a852191042c6ea5e890d2e1c10a9855
ColbyNeves1999 <colby.neves@smail.astate.edu>
2023-12-15 11:20:43 -0600

    Starting Main Pages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-import './config'; // Load environment variables
-import 'express-async-errors'; // Enable default error handling for async errors
+import './config';
+import 'express-async-errors';
 import express, { Express } from 'express';
 import session from 'express-session';
 import connectSqlite3 from 'connect-sqlite3';
+
+const app: Express = express();
+const { PORT, COOKIE_SECRET } = process.env;
+
+const SQLiteStore = connectSqlite3(session);
+
+app.use(
+    session({
+        store: new SQLiteStore({ db: 'sessions.sqlite' }),
+        secret: COOKIE_SECRET,
+        cookie: { maxAge: 8 * 60 * 60 * 1000 }, // 8 hours
+        name: 'session',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public', { extensions: ['html'] }));
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+});
